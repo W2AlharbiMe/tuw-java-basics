93c6843f852246565a5e1a511edec7ffc068af7b
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-07-25 14:32:37 +0300

    3rd question

--- day3/Main.java
+++ day3/Main.java
@@ -40,6 +40,25 @@ class Main {
         }
 
         System.out.println(output);
+
+        // 3.Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number.
+
+        do {
+            System.out.println("enter a positive number to show multiplication table for it:");
+            int number = s.nextInt();
+
+            if(number <= 0) {
+                System.out.println("please enter a positive number (it should be greater than 0 because any number multiplied by 0 is 0).");
+                continue;
+            }
+
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i + " x " + number + " = " + (i * number));
+            }
+
+            break;
+        } while(true);
         
     }
 }
