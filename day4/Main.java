d3b0d5fa698df32a4c0335bd1de6959dec4493f3
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-07-26 14:09:32 +0300

    question 2

--- day4/Main.java
+++ day4/Main.java
@@ -26,5 +26,52 @@ class Main {
             }
         }
 
+
+        // 2.Write a Java program to find the k largest elements in a given array. Elements in the array can be in any order.
+
+
+        System.out.println("How many large numbers you want to find (k): ");
+        int _k = s.nextInt();
+
+        int[] numbers = {1, 4, 17, 7, 25, 3, 100};
+
+//        another sample data.
+//        int[] numbers = {1, 4, 14, 18, 22, 33, 17, 200, 7, 25, 3, 100};
+
+
+        System.out.println("Before sort: " + Arrays.toString(numbers));
+
+//        I know this is a bad way of doing sorting. bubble sort -> O(n^3).
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 1; j < numbers.length; j++) {
+                for (int k = i; k < j; k++) {
+                    if(numbers[i] >= numbers[j]) {
+                        int tmp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        System.out.println("After sort: " + Arrays.toString(numbers));
+
+
+        int[] largest_numbers = new int[_k];
+        int counter = 0;
+        int stop_at = (_k - 1); // optimization tick.
+
+        for (int i = (numbers.length - 1); i >= 0; i--) {
+            largest_numbers[counter] = numbers[i];
+
+            if(counter == stop_at) {
+                break;
+            }
+
+            counter++;
+        }
+
+        System.out.println("Largest " + _k + " Numbers are: " + Arrays.toString(largest_numbers));
+
     }
 }
