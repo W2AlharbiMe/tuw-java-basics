bb1c6c8eabddfe414ecf05f88879af2507c60575
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-07-26 13:33:36 +0300

    question 1

--- day4/Main.java
+++ day4/Main.java
@@ -5,6 +5,22 @@ class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        
+        // 1.Write a Java program to test if the first and the last element of an array of integers are same. The length of the array must be greater than or equal to 2
+
+        int[] numbers = {60, 89, 55, 26, 55, 11, 44, 11, 33, 100, 400, 60};
+
+        // the easy approach haha
+        // System.out.println(numbers[0] == numbers[numbers.length - 1]);
+
+        // but to practice:
+        int n = numbers.length;
+        int firstElement = numbers[0];
+
+        for (int i = 0; i < n; i++) {
+            if(i == (n - 1)) {
+                System.out.println(firstElement == numbers[i]);
+            }
+        }
+
     }
 }
